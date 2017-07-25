d477ddbb7cf08bfdc8d18c0bc4f1709ff011d3e2
njogu <munyiri_james@outlook.com>
2017-07-25 12:35:23 +0300

    username as a variable

--- js/backend.js
+++ js/backend.js
@@ -4,7 +4,7 @@ var apiKey = require('./../.env').apiKey;
 Account=function() {
 
 Account.prototype.getRepos = function() {
-  $.get('https://api.github.com/users/NjoguMunyiri?access_token='+apiKey).then(function(response) {
+  $.get('https://api.github.com/users/'+username+'?access_token='+apiKey).then(function(response) {
     console.log(response);
   }).fail(function(error) {
     console.log(error.responseJSON.message);
--- js/frontend-interface.js
+++ js/frontend-interface.js
@@ -3,7 +3,7 @@ var Account = require('./../js/backend.js').accountModule;
 $(document).ready(function() {
   var currentAccObject = new Account();
   $('#submit').click(function() {
-    var acc = $('#githubacc').val();
+    var username = $('#githubacc').val();
     $('#githubacc').val("");
     currentAccObject.getRepos(acc);
   });
